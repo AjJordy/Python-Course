8cfff27ebdfd53ad609b11857e0735c54eca1781
AjJordy <jordyfaria0@gmail.com>
2023-03-14 08:20:12 -0300

    locale

--- src/modules/localization.py
+++ src/modules/localization.py
@@ -1,7 +1,8 @@
 import calendar
 import locale
 
-locale.setlocale(locale.LC_ALL, '') # ou 'pt_BR'
+locale.setlocale(locale.LC_ALL, '') # ou 'pt_BR.utf8'
 
 print(calendar.calendar(2023))
+print(locale.getlocale())
 
